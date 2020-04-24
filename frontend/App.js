87566d2a27a657d3f20e0198eee5381363158806
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-04-24 18:14:56 +0800

    Set up frontend base

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>Hola World!</div>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './frontend/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|jpg|gif)$/, use: ['file-loader'] },
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'frontend/public/index.html',
+    }),
+  ],
+};
