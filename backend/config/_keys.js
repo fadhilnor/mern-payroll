371fac4cadb7db490984685d8eef9e5fe7e08ae4
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-04-29 17:29:48 +0800

    Add secret key for JWT

--- backend/config/_keys.js
+++ backend/config/_keys.js
@@ -3,6 +3,8 @@ dbPassword =
   encodeURIComponent('YOUR_PASSWORD_HERE') +
   '@CLUSTER_NAME_HERE.mongodb.net/@COLLECTION_NAME_HERE?retryWrites=true&w=majority';
 
+secretOrKey = 'APP_SECRET_KEY';
+
 module.exports = {
   mongoURI: dbPassword,
 };
