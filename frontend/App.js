60a2da23638d1c61d91ce5aeb6416e8d295dd5cb
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-04-29 11:33:58 +0800

    Add state management (redux) store

--- frontend/App.js
+++ frontend/App.js
@@ -7,8 +7,16 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './services/authServices';
+
 import Login from './components/login.component';
 import Register from './components/register.component';
+import Dashboard from './components/dashboard.component';
 
 function Copyright() {
   return (
@@ -56,23 +64,25 @@ const styles = (theme) => ({
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Grid container component="main" className={this.props.classes.root}>
-          <CssBaseline />
-          <Grid item xs={false} sm={4} md={7} className={this.props.classes.image} />
-          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <div className={this.props.classes.paper}>
-              <Switch>
-                <Route exact path="/" component={Login} />
-                <Route path="/register" component={Register} />
-              </Switch>
-              <Box mt={5}>
-                <Copyright />
-              </Box>
-            </div>
+      <Provider store={store}>
+        <Router>
+          <Grid container component="main" className={this.props.classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={this.props.classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+              <div className={this.props.classes.paper}>
+                <Switch>
+                  <Route exact path="/" component={Login} />
+                  <Route path="/register" component={Register} />
+                </Switch>
+                <Box mt={5}>
+                  <Copyright />
+                </Box>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
